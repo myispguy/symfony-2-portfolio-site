5a2e97b85bb5e68a3b369caaeb954919b640d6dd
dave4186 <dave4186@gmail.com>
2011-07-18 16:32:08 +0000

    bare updates

--- src/DW/PortfolioBundle/Controller/DefaultController.php
+++ src/DW/PortfolioBundle/Controller/DefaultController.php
@@ -12,11 +12,6 @@ class DefaultController extends Controller
     public function indexAction()
     {
         $title = "Welcome";
-        
-        $enquiry = new Enquiry();
-        
-        var_dump($enquiry);die();
-        
         return $this->render('PortfolioBundle:Default:index.html.twig', array('page_title' => $title));
     }
 }
