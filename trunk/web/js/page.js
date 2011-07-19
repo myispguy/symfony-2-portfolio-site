1cef6d9442f1a1b3354976a9fb0cab69c076a73e
dave4186@gmail.com <dave4186@gmail.com@dcdca297-c9fb-da60-390d-fb491d2a127e>
2011-07-19 15:42:02 +0000

    JS

--- trunk/web/js/page.js
+++ trunk/web/js/page.js
@@ -12,6 +12,6 @@ var positionContainer = function() {
 	var container_height = $("#container .inner").height();
 	var window_height = $(window).height();
 
-	var margin = ((window_height - container_height) / 2) - 50;
+	var margin = (window_height - container_height) / 2;
 	$("#container").css("paddingTop", margin);
 };
